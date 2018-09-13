0a466a25eda90d9abf2dcb491241514b216ba0f3
Jordie Dooge <doog0004@hz.nl>
2018-09-13 15:20:23 +0200

    adsf
    adsf

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -22,21 +22,22 @@ echo "Attempting to build $project for OS X"
   -buildOSXUniversalPlayer "$(pwd)/asdf/Build/osx/$project.app" \
   -quit
 
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath=$(pwd)/asdf \
-  -buildLinuxUniversalPlayer "$(pwd)/asdf/Build/linux/$project.exe" \
-  -quit
+#echo "Attempting to build $project for Linux"
+#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#  -batchmode \
+#  -nographics \
+#  -silent-crashes \
+#  -logFile $(pwd)/unity.log \
+#  -projectPath=$(pwd)/asdf \
+#  -buildLinuxUniversalPlayer "$(pwd)/asdf/Build/linux/$project.exe" \
+#  -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
 
 
 echo 'Attempting to zip builds'
+echo pwd
 zip -r $(pwd)/asdf/Build/linux.zip $(pwd)/asdf/Build/linux/
 zip -r $(pwd)/asdf/Build/mac.zip $(pwd)/asdf/Build/osx/
 zip -r $(pwd)/asdf/Build/windows.zip $(pwd)/asdf/Build/windows/
