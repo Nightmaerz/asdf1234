b8634e9a8083a2871352ac572297f53e6de60f30
Jordie Dooge <doog0004@hz.nl>
2018-09-14 14:13:05 +0200

    adsf
    asdf

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -17,7 +17,7 @@ install() {
   download "$package"
 
   echo "Installing "`basename "$package"`
-  sudo installer -pkg `basename "$package"` -target /
+  sudo installer -pkg `basename "$package"` -target $(pwd)
 }
 
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
