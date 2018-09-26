db141b3a201ab4ba50d8b4ad2ac5cb878b685a57
Jordie Dooge <doog0004@hz.nl>
2018-09-26 12:42:48 +0200

    asdf
    asdf

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ echo "Attempting to build asdf1234 for Windows"
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath="$(pwd)/asdf1234" \
-  -buildWindowsPlayer "Build/windows/asdf1234.exe" \
+  -logFile $(pwd)/unitywindows.log \
+  -projectPath="$(pwd)" \
+  -buildWindowsPlayer "windows/asdf1234.exe" \
   -quit
 
 #echo "Attempting to build $project for OS X"
@@ -21,22 +21,23 @@ echo "Attempting to build asdf1234 for Windows"
 #  -buildOSXUniversalPlayer "$(pwd)/asdf/Build/osx/$project.app" \
 #  -quit
 
-echo "Attempting to build $project for Linux"
+echo "Attempting to build asdf1234 for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath="$(pwd)/asdf1234" \
-  -buildLinuxUniversalPlayer "$(pwd)/asdf/Build/linux/$project.exe" \
+  -logFile $(pwd)/unitylinux.log \
+  -projectPath="$(pwd)" \
+  -buildLinuxUniversalPlayer "linux/$project.exe" \
   -quit
 
 echo 'Logs from build'
-cat $(pwd)/unity.log
+cat $(pwd)/unitylinux.log
+cat $(pwd)/unitywindows.log
 
 
 echo 'Attempting to zip builds'
 echo $(pwd)
-zip -r $(pwd)/asdf/Build/linux.zip $(pwd)/asdf/Build/linux/
+zip -r $(pwd)/asdf/Build/linux.zip $(pwd)/linux
 #zip -r $(pwd)/asdf/Build/mac.zip $(pwd)/asdf/Build/osx/
-zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
+zip -r $(pwd)/Build/windows.zip $(pwd)/windows
